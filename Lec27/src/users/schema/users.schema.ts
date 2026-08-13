e2f00d016223e5c0a27959674f12edd0015a1094
ketevan20 <ketevansamukashvili@gmail.com>
2026-08-13 16:04:35 +0400

    task-27

---
+++ Lec27/src/users/schema/users.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema({timestamps: true})
+export class User {
+    @Prop({type: String})
+    name!: string
+
+    @Prop({type: String})
+    email!: string
+
+    @Prop({type: Number})
+    age!: number
+}
+
+// export const userSchema = SchemaFactory.createForClass(User)
+export const userSchema = SchemaFactory.createForClass(User)
---
+++ Lec27/src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/users.schema';
+import { isValidObjectId, Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.userModel.findOne({email: createUserDto.email})
+    if(existingUser) throw new BadRequestException()
+    const newUser = await this.userModel.create(createUserDto)
+    return newUser;
+  }
+
+  findAll() {
+    return this.userModel.find();
+  }
+
+  async findOne(id: string) {
+    if(!isValidObjectId(id)) throw new BadRequestException()
+    const findUserById = await this.userModel.findById(id)
+    if(!findUserById) throw new BadRequestException()
+    return findUserById;
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    console.log("axla aqane")
+    if(!isValidObjectId(id)) throw new BadRequestException()
+    console.log("aqane var")
+    const updateUserById = await this.userModel.findByIdAndUpdate(updateUserDto, {new: true})
+    if(!updateUserById) throw new BadRequestException()
+    return updateUserById;
+  }
+
+  async remove(id: string) {
+    if(!isValidObjectId(id)) throw new BadRequestException()
+    const removeUserById = await this.userModel.findByIdAndDelete(id)
+    if(!removeUserById) throw new BadRequestException()
+    return removeUserById;
+  }
+}
